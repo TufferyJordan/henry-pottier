51bf9259595e576b18bd2496ad5cb803b45dc4c6
JordanTuffery <tufferyjordan@gmail.com>
2018-05-18 05:07:45 +0200

    dave fragment state

--- HenryPottier/app/src/main/java/com/jordantuffery/henrypottier/lifecyle/activities/MainActivity.kt
+++ HenryPottier/app/src/main/java/com/jordantuffery/henrypottier/lifecyle/activities/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : BaseActivity() {
     private val libraryFragment = LibraryFragment.newInstance()
     private val shoppingListFragment = ShoppingListFragment.newInstance()
 
+    private var currentFragmentId: Int? = null
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_infos -> {
@@ -51,14 +53,44 @@ class MainActivity : BaseActivity() {
         super.onActivityResult(requestCode, resultCode, data)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(EXTRA_CURRENT_FRAGMENT_ID, currentFragmentId!!)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        restoreCurrentFragment(savedInstanceState.getInt(EXTRA_CURRENT_FRAGMENT_ID))
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
     private fun goToFragment(fragment: BaseFragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_fragment_container, fragment)
             .commitAllowingStateLoss()
+        currentFragmentId = getCurrentFragmentId(fragment)
     }
 
+    private fun getCurrentFragmentId(fragment: BaseFragment): Int = when (fragment) {
+        infoFragment -> ID_FRAGMENT_INFO
+        libraryFragment -> ID_FRAGMENT_LIBRARY
+        else -> ID_FRAGMENT_SHOPPING_LIST
+    }
+
+    private fun restoreCurrentFragment(id: Int) {
+        when (id) {
+            ID_FRAGMENT_INFO -> main_activity_bottom_bar.selectedItemId = R.id.navigation_infos
+            ID_FRAGMENT_LIBRARY -> main_activity_bottom_bar.selectedItemId = R.id.navigation_library
+            else -> main_activity_bottom_bar.selectedItemId = R.id.navigation_shopping_list
+        }
+    }
 
     companion object {
         const val REQUEST_CODE_BOOK_DETAIL_ACTIVITY = 0
+
+        private const val EXTRA_CURRENT_FRAGMENT_ID = "EXTRA_CURRENT_FRAGMENT_ID"
+
+        private const val ID_FRAGMENT_INFO = 0
+        private const val ID_FRAGMENT_LIBRARY = 1
+        private const val ID_FRAGMENT_SHOPPING_LIST = 2
     }
 }
