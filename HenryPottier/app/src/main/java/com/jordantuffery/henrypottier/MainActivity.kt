b645fecc3f8b44e9c3e337f3ac5474f2c388b77c
JordanTuffery <tufferyjordan@gmail.com>
2018-05-23 18:30:32 +0200

    refactorisation of the fragment management

--- HenryPottier/app/src/main/java/com/jordantuffery/henrypottier/MainActivity.kt
+++ HenryPottier/app/src/main/java/com/jordantuffery/henrypottier/MainActivity.kt
@@ -13,24 +13,18 @@ import kotlinx.android.synthetic.main.activity_main.main_activity_bottom_bar
 class MainActivity : BaseActivity() {
     override val layoutRes: Int = R.layout.activity_main
 
-    private val infoFragment = InfoFragment.newInstance()
-    private val libraryFragment = LibraryFragment.newInstance()
-    private val shoppingListFragment = ShoppingListFragment.newInstance()
-
-    private var currentFragmentId: Int? = null
-
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_infos -> {
-                goToFragment(infoFragment)
+                goToFragment(InfoFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_library -> {
-                goToFragment(libraryFragment)
+                goToFragment(LibraryFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_shopping_list -> {
-                goToFragment(shoppingListFragment)
+                goToFragment(ShoppingListFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -53,13 +47,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt(EXTRA_CURRENT_FRAGMENT_ID, currentFragmentId!!)
+        outState.putInt(EXTRA_CURRENT_NAVIGATION_ID, main_activity_bottom_bar.selectedItemId)
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        restoreCurrentFragment(savedInstanceState.getInt(
-            EXTRA_CURRENT_FRAGMENT_ID))
+        main_activity_bottom_bar.selectedItemId = savedInstanceState.getInt(
+            EXTRA_CURRENT_NAVIGATION_ID)
         super.onRestoreInstanceState(savedInstanceState)
     }
 
@@ -67,30 +61,11 @@ class MainActivity : BaseActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_fragment_container, fragment)
             .commitAllowingStateLoss()
-        currentFragmentId = getCurrentFragmentId(fragment)
-    }
-
-    private fun getCurrentFragmentId(fragment: BaseFragment): Int = when (fragment) {
-        infoFragment -> ID_FRAGMENT_INFO
-        libraryFragment -> ID_FRAGMENT_LIBRARY
-        else -> ID_FRAGMENT_SHOPPING_LIST
-    }
-
-    private fun restoreCurrentFragment(id: Int) {
-        when (id) {
-            ID_FRAGMENT_INFO -> main_activity_bottom_bar.selectedItemId = R.id.navigation_infos
-            ID_FRAGMENT_LIBRARY -> main_activity_bottom_bar.selectedItemId = R.id.navigation_library
-            else -> main_activity_bottom_bar.selectedItemId = R.id.navigation_shopping_list
-        }
     }
 
     companion object {
         const val REQUEST_CODE_BOOK_DETAIL_ACTIVITY = 0
 
-        private const val EXTRA_CURRENT_FRAGMENT_ID = "EXTRA_CURRENT_FRAGMENT_ID"
-
-        private const val ID_FRAGMENT_INFO = 0
-        private const val ID_FRAGMENT_LIBRARY = 1
-        private const val ID_FRAGMENT_SHOPPING_LIST = 2
+        private const val EXTRA_CURRENT_NAVIGATION_ID = "EXTRA_CURRENT_NAVIGATION_ID"
     }
 }
